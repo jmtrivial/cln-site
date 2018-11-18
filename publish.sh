a7723fd89192307b081eed02c62407467c253e24
Jean-Marie Favreau <jean-marie.favreau@uca.fr>
2018-11-18 22:38:07 +0100

    Correction du script

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-make && echo rsync -rav build/*.html jmtrivial.info:~/cln/ && { 
+make && rsync -av build/*.html jmtrivial.info:~/cln/ && { 
 for i in css doc documents images js pdf; do
-  echo rsync -rav $i/ jmtrivial.info:~/cln/$i/ 
+  rsync -rav $i/ jmtrivial.info:~/cln/$i/ 
 done }
