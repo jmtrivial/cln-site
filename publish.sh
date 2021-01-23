2a87aa5ea3d9d28698282b575c9d754752b27c10
Jean-Marie Favreau <j-marie.favreau@uca.fr>
2021-01-23 18:18:40 +0100

    Exécution parallèle

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git push
-make && rsync -av build/*.html murol:/var/www/cln.jmfavreau.info/ && { 
+make -j && rsync -av build/*.html murol:/var/www/cln.jmfavreau.info/ && { 
 for i in css docs images js pdf; do
   rsync -rav $i/ murol:/var/www/cln.jmfavreau.info/$i/ 
 done }
