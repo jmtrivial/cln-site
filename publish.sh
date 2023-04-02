7c7592263e771b09da878ba7ae8d4ab1b7f0dc54
Jean-Marie Favreau <j-marie.favreau@uca.fr>
2023-04-02 14:36:46 +0200

    mise à jour du script de publication

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git push
-make -j && rsync -av build/*.html jmtrivial.info:/var/www/cln.jmfavreau.info/ && { 
+make -j && rsync -av build/*.html build/*json jmtrivial.info:/var/www/cln.jmfavreau.info/ && {
 for i in css docs images js pdf; do
   rsync -rav $i/ jmtrivial.info:/var/www/cln.jmfavreau.info/$i/ 
 done }
